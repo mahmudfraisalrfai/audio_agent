a8f415c15e3cca29ca4338e75fdc546f212225cd
mahmoud <mahmoudalrigae@gmail.com>
2025-04-25 02:24:14 +0300

    step end

--- app/lib/actions/ations.ts
+++ app/lib/actions/ations.ts
@@ -3,8 +3,9 @@ import postgres from "postgres";
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 export async function getUserById(): Promise<information> {
-  const data =
-    await sql<any>`SELECT * FROM interview ORDER BY createdat DESC LIMIT 1`;
+  const data = await sql<
+    information[]
+  >`SELECT * FROM interview ORDER BY createdat DESC LIMIT 1`;
 
   const item = data[0];
 

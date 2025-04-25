55c94997ae059a3da2ac6edb223118097c5b008c
mahmoud <mahmoudalrigae@gmail.com>
2025-04-25 03:31:33 +0300

    step end

--- app/lib/actions/ations.ts
+++ app/lib/actions/ations.ts
@@ -1,3 +1,6 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
 import postgres from "postgres";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
@@ -9,6 +12,7 @@ export async function getUserById(): Promise<information> {
 
   const item = data[0];
 
+  revalidatePath("/");
   return {
     ...item,
   };

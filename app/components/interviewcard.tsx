78232da7449b479dc07e57e15ce78bfa34955a5a
mahmoud <mahmoudalrigae@gmail.com>
2025-04-26 04:22:32 +0300

    end

--- app/components/interviewcard.tsx
+++ app/components/interviewcard.tsx
@@ -1,6 +1,5 @@
 import { getUserById } from "@/app/lib/actions/actions";
 import Card from "./card";
-("@/app/components/card");
 export default async function interviewcard() {
   const interview: information = await getUserById();
 
